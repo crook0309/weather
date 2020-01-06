877cfc709ffb0243f2c8ba5851e48f3bd09edd4d
crook0309 <crook0309@gmail.com>
2020-01-06 10:52:08 +0200

    second commit

---
+++ precache-manifest.216777751acb887ab71a21871872c7bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c216143334a2b057327e3243e549f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df88da05beecf91f7381",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "4aff9d89f3a12961a364",
+    "url": "/static/css/main.3724baee.chunk.css"
+  },
+  {
+    "revision": "df88da05beecf91f7381",
+    "url": "/static/js/2.db116194.chunk.js"
+  },
+  {
+    "revision": "4aff9d89f3a12961a364",
+    "url": "/static/js/main.32bed7fd.chunk.js"
+  },
+  {
+    "revision": "3e95bd9de01710d43a49",
+    "url": "/static/js/runtime-main.d84ea1a8.js"
+  },
+  {
+    "revision": "a5eb16ad33dab640abbed0eed673bb31",
+    "url": "/static/media/WeatherLogo.a5eb16ad.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
